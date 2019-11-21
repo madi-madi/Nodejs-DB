60cf60759b890036ccdee987cc29a6ccbeecd8f3
madi-madi <ibrahim.s.m.2010@gmail.com>
2019-11-21 08:56:56 +0200

    use routes in index.js

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 // FileName: index.js
 // Import express
 let express = require('express')
+// Import routes
+let apiRoutes = require("./api-routes")
 // Initialize the app
 let app = express();
 // Setup server port
 var port = process.env.PORT || 8080;
+// Use Api routes in the App
+app.use('/api', apiRoutes)
 // Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
 // Launch app to listen to specified port
